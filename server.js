a564e79d3513485d446ec351a407c1c08ccbc6e2
Brian-Mas <brianmasschaele@icloud.com>
2016-11-02 20:55:46 +0100

    Commented a lot

--- server.js
+++ server.js
@@ -33,75 +33,75 @@ function handleError(res, reason, message, code){
     res.status(code || 500).json({"error" : message});
 }
 
-app.get('/contacts', function(req, res) {
-    db.collection(CONTACTS_COLLECTION).find({}).toArray(function (err, docs) {
-        if (err){
-            handleError(res, err.message, "Failed to get contacts.");
-        }else{
-            res.status(200).json(docs);
-        }
-    });
-});
-
-app.post('/contacts', function(req, res) {
-    var newContact = req.body;
-    newContact.createDate = new Date();
-
-    if (!(req.body.firstName || req.body.lastName)){
-        handleError(res, "Invalid user input", "Must provide a first or last name", 400);
-    }
-
-    db.collection(CONTACTS_COLLECTION).insertOne(newContact, function (err, doc) {
-        if (err){
-            handleError(res, err.message, "Failed to create new contact.");
-        }else {
-            res.status(201).json(doc.ops[0]);
-        }
-    });
-});
-
-/*  "/contacts/:id"
- *    GET: find contact by id
- *    PUT: update contact by id
- *    DELETE: deletes contact by id
- */
-
-app.get("/contacts/id/:id", function(req, res) {
-    db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
-        if (err) {
-            handleError(res, err.message, "Failed to get contact");
-        } else {
-            res.status(200).json(doc);
-        }
-    });
-});
-
-// app.put('/contacts/:id', function (req, res) {
-//     var updateDoc = req.body;
-//     delete updateDoc._id;
-//
-//     db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function (err, doc) {
+// app.get('/contacts', function(req, res) {
+//     db.collection(CONTACTS_COLLECTION).find({}).toArray(function (err, docs) {
 //         if (err){
-//             handleError(res, err.message, "Failed to update contact");
+//             handleError(res, err.message, "Failed to get contacts.");
 //         }else{
-//             res.status(204).end
+//             res.status(200).json(docs);
 //         }
-//     })
+//     });
 // });
-
-// app.delete('/contacts/:id', function (req, res) {
 //
+// app.post('/contacts', function(req, res) {
+//     var newContact = req.body;
+//     newContact.createDate = new Date();
+//
+//     if (!(req.body.firstName || req.body.lastName)){
+//         handleError(res, "Invalid user input", "Must provide a first or last name", 400);
+//     }
+//
+//     db.collection(CONTACTS_COLLECTION).insertOne(newContact, function (err, doc) {
+//         if (err){
+//             handleError(res, err.message, "Failed to create new contact.");
+//         }else {
+//             res.status(201).json(doc.ops[0]);
+//         }
+//     });
 // });
-
-app.get('/firstName/:firstName', function (req, res) {
-    db.collection(CONTACTS_COLLECTION).findOne({firstName: req.params.firstName}, function(err, doc) {
-        if(err){
-            handleError(res, err.message, "Failed to get contact");
-        }else {
-            res.status(200).json(doc);
-        }
-    });
-});
+//
+// /*  "/contacts/:id"
+//  *    GET: find contact by id
+//  *    PUT: update contact by id
+//  *    DELETE: deletes contact by id
+//  */
+//
+// app.get("/contacts/id/:id", function(req, res) {
+//     db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+//         if (err) {
+//             handleError(res, err.message, "Failed to get contact");
+//         } else {
+//             res.status(200).json(doc);
+//         }
+//     });
+// });
+//
+// // app.put('/contacts/:id', function (req, res) {
+// //     var updateDoc = req.body;
+// //     delete updateDoc._id;
+// //
+// //     db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function (err, doc) {
+// //         if (err){
+// //             handleError(res, err.message, "Failed to update contact");
+// //         }else{
+// //             res.status(204).end
+// //         }
+// //     })
+// // });
+//
+// // app.delete('/contacts/:id', function (req, res) {
+// //
+// // });
+//
+// app.get('/firstName/:firstName', function (req, res) {
+//     db.collection(CONTACTS_COLLECTION).findOne({firstName: req.params.firstName}, function(err, doc) {
+//         if(err){
+//             handleError(res, err.message, "Failed to get contact");
+//         }else {
+//             res.status(200).json(doc);
+//         }
+//     });
+// });
 
 app.get('/api/landmarks', function(req, res) {
     db.collection(LANDMARKS_COLLECTION).find({}).toArray(function (err, docs) {
