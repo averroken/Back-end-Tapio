1394022f820e2ef58f843c4834868eee8a0744f4
Brian-Mas <brianmasschaele@icloud.com>
2016-11-04 22:54:57 +0100

    Cleaned up + fix
    Deleted commented code
    Deleted unnecessary api/map post
    Fixed api/map get (collection was wrong)

--- server.js
+++ server.js
@@ -34,76 +34,6 @@ function handleError(res, reason, message, code){
     res.status(code || 500).json({"error" : message});
 }
 
-// app.get('/contacts', function(req, res) {
-//     db.collection(CONTACTS_COLLECTION).find({}).toArray(function (err, docs) {
-//         if (err){
-//             handleError(res, err.message, "Failed to get contacts.");
-//         }else{
-//             res.status(200).json(docs);
-//         }
-//     });
-// });
-//
-// app.post('/contacts', function(req, res) {
-//     var newContact = req.body;
-//     newContact.createDate = new Date();
-//
-//     if (!(req.body.firstName || req.body.lastName)){
-//         handleError(res, "Invalid user input", "Must provide a first or last name", 400);
-//     }
-//
-//     db.collection(CONTACTS_COLLECTION).insertOne(newContact, function (err, doc) {
-//         if (err){
-//             handleError(res, err.message, "Failed to create new contact.");
-//         }else {
-//             res.status(201).json(doc.ops[0]);
-//         }
-//     });
-// });
-//
-// /*  "/contacts/:id"
-//  *    GET: find contact by id
-//  *    PUT: update contact by id
-//  *    DELETE: deletes contact by id
-//  */
-//
-// app.get("/contacts/id/:id", function(req, res) {
-//     db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
-//         if (err) {
-//             handleError(res, err.message, "Failed to get contact");
-//         } else {
-//             res.status(200).json(doc);
-//         }
-//     });
-// });
-//
-// // app.put('/contacts/:id', function (req, res) {
-// //     var updateDoc = req.body;
-// //     delete updateDoc._id;
-// //
-// //     db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function (err, doc) {
-// //         if (err){
-// //             handleError(res, err.message, "Failed to update contact");
-// //         }else{
-// //             res.status(204).end
-// //         }
-// //     })
-// // });
-//
-// // app.delete('/contacts/:id', function (req, res) {
-// //
-// // });
-//
-// app.get('/firstName/:firstName', function (req, res) {
-//     db.collection(CONTACTS_COLLECTION).findOne({firstName: req.params.firstName}, function(err, doc) {
-//         if(err){
-//             handleError(res, err.message, "Failed to get contact");
-//         }else {
-//             res.status(200).json(doc);
-//         }
-//     });
-// });
-
 app.get('/api/landmarks', function(req, res) {
     db.collection(LANDMARKS_COLLECTION).find({}).toArray(function (err, docs) {
         if (err){
@@ -133,7 +63,7 @@ app.post('/api/landmarks', function(req, res) {
 
 
 app.get('/api/map', function(req, res) {
-    db.collection(MAP_COLLECTION).find({
+    db.collection(LANDMARKS_COLLECTION).find({
     },{
         "Type": 1,
         "Locatie" : 1
@@ -142,23 +72,6 @@ app.get('/api/map', function(req, res) {
             handleError(res, err.message, "Failed to get contacts.");
         }else{
             res.status(200).json(docs);
-        }
-    });
-});
-
-app.post('/api/map', function(req, res) {
-    var newMap = req.body;
-    newMap.createDate = new Date();
-
-    if (!(req.body.Type || !req.body.Locatie)){
-        handleError(res, "Invalid user input", "Must provide a name and discription", 400);
-    }
-
-    db.collection(MAP_COLLECTION).insertOne(newMap, function (err, doc) {
-        if (err){
-            handleError(res, err.message, "Failed to create new contact.");
-        }else {
-            res.status(201).json(doc.ops[0]);
         }
     });
 });
